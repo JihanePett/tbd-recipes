c0af391f3b04617fd76f97d2fc8070bc5c5f3d0f
JihanePett <jihane.petticrew@gmail.com>
2020-09-23 16:34:51 +0000

    updated flash messages

--- app.py
+++ app.py
@@ -146,10 +146,8 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(
-                            request.form.get("username")))
                 return redirect(url_for(
-                            "profile", username=session["user"]))
+                            "get_recipes", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -181,7 +179,6 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
-
 
 
 @app.route('/get_categories')
